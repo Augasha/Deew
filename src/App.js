ce4cd1ff83be0b8de511a02b756e9f923a88bd1f
Augasha <3higher04@gmail.com>
2024-11-18 07:54:53 +0300

    imported all the files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Header from './Components/Header/Header'
+import Categories from './Components/Categories/Categories'
+import Shop from './Components/Shop/Shop'
+import Deal from './Components/Deal/Deal'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Blog from './Components/Blog/Blog'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
     <main className='app'>
       <Navbar />
+      <Header />
+      <Categories />
+      <Shop />
+      <Deal />
+      <Testimonials />
+      <Blog />
+      <Footer />
     </main>
   )
 }
