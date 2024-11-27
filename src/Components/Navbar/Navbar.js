5f3a49bea2a8a79b454739f09f03146e6547f03f
Augasha <3higher04@gmail.com>
2024-11-27 09:38:27 +0300

    added new divs

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
     <header>
       <nav>
         <div className='tn-container container'>
-          
+          <a href='' className='logo'>shopee</a>
+          <div className='search-box'>
+            <input type='search' name='search' id='' placeholder='Search For a 3higherKush'/>
+            <div className='search-icon'></div>
+          </div>
         </div>
       </nav>
     </header>
